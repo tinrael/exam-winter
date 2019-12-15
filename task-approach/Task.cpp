a0d488c4fbc722e5f6b34abc3fe145c9d6f5f587
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2019-12-15 14:12:07 +0200

    Added the unique id for each task

--- task-approach/Task.cpp
+++ task-approach/Task.cpp
@@ -1,11 +1,20 @@
 #include "Task.h"
 
+int Task::nextId = 0;
+
 Task::Task(const string& type, unsigned size) : type(type), size(size)
 {
+	id = nextId;
+	nextId++;
 }
 
 Task::~Task()
 {
+}
+
+int Task::getId() const
+{
+	return id;
 }
 
 string Task::getType() const
--- task-approach/Task.h
+++ task-approach/Task.h
@@ -7,6 +7,8 @@ using std::string;
 class Task
 {
 private:
+	static int nextId;
+	int id;
 	string type;
 	unsigned size;
 
@@ -14,6 +16,7 @@ public:
 	Task(const string &type, unsigned size);
 	virtual ~Task();
 	
+	int getId() const;
 	string getType() const;
 	unsigned getSize() const;
 };
