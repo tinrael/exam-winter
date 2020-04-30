be90eed8c527e455cbd9448a7488c0e653488d53
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-04-30 18:27:40 +0300

    Add the test case

--- unit-tests/tests-approach.cpp
+++ unit-tests/tests-approach.cpp
@@ -1,8 +1,17 @@
 #include "catch.hpp"
 
+#include "Task.h"
 #include "Approach.h"
 
-TEST_CASE("task type", "[approach]") {
-	ApproachTypeA approach("square", 0.3, true, 35);
-	REQUIRE(approach.getTaskType() == "square");
+TEST_CASE("approaches can be usable and unusable to the task", "[approach]") {
+	Task task1("square", 20);
+	Task task2("matrix addition", 10);
+
+	ApproachTypeA approach1("square", 0.3, true, 35);
+	REQUIRE(approach1.isUsable(task1));
+	REQUIRE_FALSE(approach1.isUsable(task2));
+
+	ApproachTypeA approach2("square", 0, true, 35);
+	REQUIRE_FALSE(approach2.isUsable(task1));
+	REQUIRE_FALSE(approach2.isUsable(task2));
 }
