5ebb355225147ffdad573f37c3f6ab2f176f1683
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-04-30 19:09:54 +0300

    Add one more use case

--- unit-tests/tests-approach.cpp
+++ unit-tests/tests-approach.cpp
@@ -14,4 +14,20 @@ TEST_CASE("approaches can be usable and unusable to the task", "[approach]") {
 	ApproachTypeA approach2("square", 0, true, 35);
 	REQUIRE_FALSE(approach2.isUsable(task1));
 	REQUIRE_FALSE(approach2.isUsable(task2));
+
+	ApproachTypeB approach3("square", 0.2, true, 40);
+	REQUIRE(approach3.isUsable(task1));
+	REQUIRE_FALSE(approach3.isUsable(task2));
+}
+
+TEST_CASE("approaches of type C decrease success probability if this certain task solves again", "[approach]") {
+	Task task1("square", 20);
+	Task task2("square", 35);
+	ApproachTypeC approach("square", 0.5, true, 10);
+
+	REQUIRE(approach.getSuccessProbability(task1) == Approx(0.5));
+	approach.solve(task1);
+	REQUIRE(approach.getSuccessProbability(task1) < 0.5);
+
+	REQUIRE(approach.getSuccessProbability(task2) == Approx(0.5));
 }
