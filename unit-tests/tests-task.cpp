5e481902b167f5fec7b819ff998ad61f9143068e
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-04-30 18:27:19 +0300

    Delete getId REQUIREs
    As the id data member use private static data member nextId, it is hard to run multiple tests when getId REQUIREs exist. Run with getId REQUIREs require a certain order of tests execution.

--- unit-tests/tests-task.cpp
+++ unit-tests/tests-task.cpp
@@ -4,22 +4,18 @@
 
 TEST_CASE("initialization and assignment", "[task]") {
 	Task task1("square", 50);
-	REQUIRE(task1.getId() == 0);
 	REQUIRE(task1.getType() == "square");
 	REQUIRE(task1.getSize() == 50);
 
 	Task task2 = task1;
-	REQUIRE(task2.getId() == 0);
 	REQUIRE(task2.getType() == "square");
 	REQUIRE(task2.getSize() == 50);
 
 	Task task3("perimeter", 20);
-	REQUIRE(task3.getId() == 1);
 	REQUIRE(task3.getType() == "perimeter");
 	REQUIRE(task3.getSize() == 20);
 
 	task2 = Task("matrix addition", 45);
-	REQUIRE(task2.getId() == 2);
 	REQUIRE(task2.getType() == "matrix addition");
 	REQUIRE(task2.getSize() == 45);
 }
